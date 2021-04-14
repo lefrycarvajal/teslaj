3562038e086a2f6c5cdc3a034f7a61ea5d4d4b86
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2021-04-13 21:25:59 -0400

    Update conexiondb.php

--- conexiondb.php
+++ conexiondb.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","tesladb");
+$conex = mysqli_connect("grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","riupb1asv37n5o1m","d85hohubforehe7e","i84qmsfn2afjmp6w");
 
 ?>
